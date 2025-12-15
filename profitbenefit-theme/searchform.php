4dcaa07bbb71a46c5ed25266cae8c6357a1119d2
mahendragautam <47153931+mahendragautam@users.noreply.github.com>
2025-12-15 16:18:27 +0545

    Delete profitbenefit-theme directory

--- profitbenefit-theme/searchform.php
+++
@@ -1,4 +0,0 @@
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <input type="search" class="search-input" placeholder="<?php echo esc_attr_x( 'Search articles...', 'placeholder', 'profitbenefit-theme' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-    <button type="submit" class="search-btn">🔍</button>
-</form>
